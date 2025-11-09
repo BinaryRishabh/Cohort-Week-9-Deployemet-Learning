1bac12bd0d3a6874f32af8357eec35ed853af932
Rishabh0901 <srivastavarishabh2001@gmail.com>
2025-11-09 17:54:56 +0530

    changes in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.get('/', async (req, res) => {
 
 app.get('/print', (req, res) => {
     res.send(`
-        <h1> Hello everyone this is Rishabh's new Website </h1>
+        <h1> Hello everyone. </h1>
         <p> How are feeling? Are you excited! </p>
     `);
 });
